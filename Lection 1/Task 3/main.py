945297409d0e0db77ef085249667a374764132c2
b.zhiganov <146016485+Zhiganchik@users.noreply.github.com>
2023-10-10 12:55:05 +0300

    completed task 3

--- Lection 1/Task 3/main.py
+++ Lection 1/Task 3/main.py
@@ -1,15 +1,23 @@
-x = ("Converter temperature")
-print (x)
+x = "Converter temperature"
+print(x)
+
 def celcius_in_Fahrenheit(Cel):
-    return (Cel * 95 / 5) + 32
-def Fahrenheit_in_Fahrenheit(Far):
-    return (Far - 32) * 5/9
-if conversion == "Cel in Far":
-    Cel = float(input("Load t in Cel: "))
+    return (Cel * 9 / 5) + 32
+def Fahrenheit_in_celcius(Far):
+    return (Far - 32) * 5 / 9
+
+option_1 = "1"
+option_2 = "2"
+
+conversion = input("what should be done ('1' for 'Cel in Far' or '2' for 'Far in Cel'): ")
+
+if conversion == "1":
+    Cel = float(input("Load temperature in Cel: "))
     Far = celcius_in_Fahrenheit(Cel)
     print (str(Cel) + "degrees celcius equals" + str(Far) + "degrees in Fahrenheit")
-elif conversion == "Far in Cel":
-    Far = float(input("Load t in Far: "))
-    Cel = Fahrenheit_in_Fahrenheit(Far)
+
+elif conversion == "2":
+    Far = float(input("Load temperature in Far: "))
+    Cel = Fahrenheit_in_celcius(Far)
     print (str(Far) + "degrees Fahrenheit equals" + str(Cel) + "degrees in celcius")
 
